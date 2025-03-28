7cd63c91ea7a145fb8d2ddcd40fd6cc5d43f3df3
trsaints <thiago.dev2k3@gmail.com>
2025-03-27 23:34:45 -0300

    update: add valiation attributes regarding content restrictions for database entries

--- MoviesApi/Data/Models/Movie.cs
+++ MoviesApi/Data/Models/Movie.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+
 namespace MoviesApi.Data.Models;
 
 
 public class Movie
 {
+	[Required]
+	[MaxLength(128)]
 	public string? Title { get; set; }
+	
+	[Required]
+	[MaxLength(128)]
 	public string? Genre { get; set; }
+	
+	[Required]
+	[Range(70, 600)]
 	public uint Duration { get; set; }
 }
